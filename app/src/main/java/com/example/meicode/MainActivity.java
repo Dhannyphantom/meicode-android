08ea1b146754d7dec05e407be3e225bfee0231a2
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-05-01 13:52:24 +0100

    RadioButtons

--- app/src/main/java/com/example/meicode/MainActivity.java
+++ app/src/main/java/com/example/meicode/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.meicode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     CheckBox checkBoxHarry, checkBoxJoker, checkBoxMatrix;
+    RadioGroup radioGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         checkBoxHarry = findViewById(R.id.checkboxHarryporter);
+        radioGender = findViewById(R.id.rgGender);
+        radioGender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i) {
+                    case R.id.rbMale:
+                        Toast.makeText(MainActivity.this, "Male", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.rbFemale:
+                        Toast.makeText(MainActivity.this, "Female", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.rbOther:
+                        Toast.makeText(MainActivity.this, "What the heck are you!", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
 
         checkBoxHarry.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
