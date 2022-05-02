668763c448abf9d0dfc25fed831490a4f4e39fbb
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-05-02 23:33:45 +0100

    Floating Action Buttons

--- app/src/main/java/com/example/meicode/MainActivity.java
+++ app/src/main/java/com/example/meicode/MainActivity.java
@@ -1,42 +1,25 @@
 package com.example.meicode;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+    FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.settingsMenu:
-                Toast.makeText(this, "Settings clicked", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.deleteMenu:
-                Toast.makeText(this, "Delete icon clicked", Toast.LENGTH_SHORT).show();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-
+        fab = findViewById(R.id.mainFlow);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Floated", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
