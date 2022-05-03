31798f4c406afa56837f54a0aea4980b7eea9508
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-05-03 13:52:10 +0100

    Material CardViews

--- app/src/main/java/com/example/meicode/MainActivity.java
+++ app/src/main/java/com/example/meicode/MainActivity.java
@@ -1,48 +1,31 @@
 package com.example.meicode;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.annotation.SuppressLint;
+
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.card.MaterialCardView;
 
 public class MainActivity extends AppCompatActivity {
-    private Button snackBtn;
-    private RelativeLayout parentLayout;
+
+    private MaterialCardView cdUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        snackBtn = findViewById(R.id.snackBtn);
-        parentLayout= findViewById(R.id.parentLayout);
-
-        snackBtn.setOnClickListener(new View.OnClickListener() {
+        cdUser = findViewById(R.id.cdUser);
+        cdUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showSnackbar();
+                Toast.makeText(MainActivity.this,"Clicked", Toast.LENGTH_LONG).show();
             }
         });
 
-    }
-
-    private void showSnackbar () {
-        Snackbar.make(parentLayout,"This is a snackbar", Snackbar.LENGTH_INDEFINITE)
-                .setAction("Retry", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Toast.makeText(MainActivity.this, "Retried", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .setActionTextColor(Color.GREEN)
-                .setTextColor(Color.MAGENTA)
-                .setBackgroundTint(Color.CYAN)
-                .show();
     }
 
 }
